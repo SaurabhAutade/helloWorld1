37959aa734bd8c28ca4bbf971cbf310eb41e78f6
Saurabh Autade <saurabhautade99@gmail.com>
2023-05-08 17:03:10 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine(Sum(10, 20));
+            Console.WriteLine(Sub(20, 10));
+            Console.WriteLine(Mul(10, 20));
+
+        }
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+
+
+
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+
+}
